90edd3ce548aebfbadec001026f5ee09328fb73a
Kobong <wangkobong@gmail.com>
2021-11-29 20:55:06 +0900

    카카오 apikey 추가

--- CanISoccer/Extension/CanISoccer++Bundle.swift
+++ CanISoccer/Extension/CanISoccer++Bundle.swift
@@ -31,5 +31,13 @@ extension Bundle {
         guard let key = resouce["openweatherAPIKEY"] as? String else { fatalError("WeatherInfo.plist에 openweatherAPIKEY를 설정해주세요.")}
         return key
     }
+    
+    var kakaoAPIKEY: String {
+        guard let file = self.path(forResource: K.WeatherInfoPlist, ofType: "plist") else { return ""}
+        
+        guard let resouce = NSDictionary(contentsOfFile: file) else { return ""}
+        guard let key = resouce["openweatherAPIKEY"] as? String else { fatalError("WeatherInfo.plist에 Authorization를 설정해주세요.")}
+        return key
+    }
 }
 
