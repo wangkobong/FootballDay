c3a9d6d42768ce7aab9e9f194d2632945825a52f
Kobong <wangkobong@gmail.com>
2021-12-03 01:49:02 +0900

    setting뷰 추가

---
+++ CanISoccer/Controllers/SettingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingViewController.swift
+//  CanISoccer
+//
+//  Created by sungyeon kim on 2021/12/02.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
