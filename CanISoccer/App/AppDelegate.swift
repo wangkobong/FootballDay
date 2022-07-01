6a203c2b341550761a06338898ef2a4f70a8aac4
wangkobong <wangkobong@gmail.com>
2022-07-01 15:27:23 +0900

    project: SceneDelegate에 AppCoordinator 추가

--- CanISoccer/App/AppDelegate.swift
+++ CanISoccer/App/AppDelegate.swift
@@ -38,6 +38,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
         return UIInterfaceOrientationMask.portrait
     }
+    
 
 }
 
