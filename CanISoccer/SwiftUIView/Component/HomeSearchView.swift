8179665b9c31202eb34d2da4d9e07f02a6fca0d9
wangkobong <wangkobong@gmail.com>
2022-10-12 23:33:49 +0900

    State 주석 추가

--- CanISoccer/SwiftUIView/Component/HomeSearchView.swift
+++ CanISoccer/SwiftUIView/Component/HomeSearchView.swift
@@ -8,6 +8,15 @@
 import SwiftUI
 
 struct HomeSearchView: View {
+    
+    /*
+     State:
+     사용자가 상호작용을 통해 상태를 업데이트 하면 UI는 상태에 따라서 자동으로 업데이트 됨
+     상태관리가 UIKit보다 비교적 쉽고, UI와 상태가 동기화되지 않는 문제도 거의 발생되지 않는다.
+     스유에서는 뷰를 직접 업데이트하지 않는다. 상태를 바꾸면 자동으로 뷰가 업데이트 됨.
+     값이 바뀔때마다 UI가 업데이트 되어야 한다면 @State로 변수를 선언.
+     
+     */
     @State private var address = ""
     var body: some View {
         HStack {
